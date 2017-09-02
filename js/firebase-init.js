17f0d36d671ba5ea342c4bb63811af10c2d1f724
Kayla <kaylabaek@coupang.com>
2017-09-02 13:55:20 +0900

    firebase json 데이타 연동

---
+++ js/firebase-init.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDf5Dzgjf_OBC6uP6Zbz9Dd7oHScXBPJSg",
+  authDomain: "yeogaemogaco-42961.firebaseapp.com",
+  databaseURL: "https://yeogaemogaco-42961.firebaseio.com",
+  storageBucket: "yeogaemogaco-42961.appspot.com"
+};
+firebase.initializeApp(config);
